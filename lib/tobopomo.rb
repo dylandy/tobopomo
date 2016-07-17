f3b5da3eb9efefbf1e5c1d3b84832b3f433efffe
dylandy.chang <dylandy.chang@gmail.com>
2016-07-18 01:30:58 +0800

    finished tobopomo function and added keymap into project

--- lib/tobopomo.rb
+++ lib/tobopomo.rb
@@ -1,18 +1,19 @@
 require "tobopomo/version"
 require "tobopomo/data"
+require "tobopomo/keymap"
 
 module Tobopomo
-  def self.normalise(input)
-    puts input
-  end
-  def self.char_seperater()
+  def normalise(string)
+    string.gsub(/\s+/, "")
   end
   def tobopomo(input)
-    Tobopomo::normalise(input)
-    puts $tsi["ㄨㄛˇ"]
+    total = 0
+    input = input.chars.map{|x| $layout[:"#{x.ord.to_s}"]}.join("")
+    string = input.split(/[" "|"ˇ"|"ˋ"|"ˊ"|"˙"]/)
+    string = string.map.with_index{|x , index| total = total + x.length;x+input[total + index]}
+    return string.map{|x| normalise(x)}
   end
   def tokanji(input)
-    Tobopomo::normalise(input)
   end
 end
 include Tobopomo
