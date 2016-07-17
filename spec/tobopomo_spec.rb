c1ed57c4a21fdd4aaca44af4d83e63a0888fac4d
dylandy.chang <dylandy.chang@gmail.com>
2016-07-18 01:59:38 +0800

    finished tobopomo test script

--- spec/tobopomo_spec.rb
+++ spec/tobopomo_spec.rb
@@ -18,11 +18,36 @@ describe Tobopomo do
     case_ten = "5;32k6ep 284gj4u u;4el "
 
     it 'first element first tone test' do
-      print tobopomo("su3cl3")
       expect(tobopomo(case_one)).to eq(["ㄏㄠ","ㄏㄠ","ㄨㄢˊ"])
     end
 
-    it 'case two' do
+    it 'first and last element are both in first tone test' do
+      expect(tobopomo(case_two)).to eq([ 'ㄏㄟ', 'ㄛ' ])
+    end
+
+    it 'first and last element are both in first tone; however, there are some other tone characters in the middle' do
+      expect(tobopomo(case_three)).to eq([ 'ㄎㄜ', 'ㄗˇ', 'ㄐㄧㄢ' ])
+    end
+    it 'last element has several first tone characters' do
+      expect(tobopomo(case_four)).to eq([ 'ㄍㄨㄞˋ', 'ㄕㄨ', 'ㄕㄨ' ])
+    end
+    it 'middle character is first tone but not for first and last element' do
+      expect(tobopomo(case_five)).to eq([ 'ㄒㄧㄠˇ', 'ㄏㄨㄟ', 'ㄒㄧㄚˊ' ])
+    end
+    it 'long sentence with a single first tone in the middle' do
+      expect(tobopomo(case_six)).to eq([ 'ㄨㄛˇ', 'ㄕˋ', 'ㄎㄜ', 'ㄒㄩㄝˊ', 'ㄍㄨㄞˋ', 'ㄖㄣˊ' ])
+    end
+    it 'first character is first tone and the second character with other tone contenting three symbols and first two symbols cannot form a proper character.' do
+      expect(tobopomo(case_seven)).to eq([ 'ㄧ', 'ㄋㄧㄢˊ' ])
+    end
+    it 'it should return one to eight' do
+      expect(tobopomo(case_eight)).to eq([ 'ㄧ', 'ㄦˋ', 'ㄙㄢ', 'ㄙˋ', 'ㄨˇ', 'ㄌㄧㄡˋ','ㄑㄧ','ㄅㄚ' ])
+    end
+    it 'some null hide in long sentence' do
+      expect(tobopomo(case_nine)).to eq([ 'ㄓㄤˇ', 'ㄉㄜˊ', 'ㄍㄣ', 'ㄉㄚˋ', 'ㄕㄨˋ', 'ㄧˊ', 'ㄧㄤˋ','ㄍㄠ' ])
+    end
+    it 'some null hide in long sentence as well' do
+      expect(tobopomo(case_ten)).to eq([ 'ㄓㄤˇ', 'ㄉㄜˊ', 'ㄍㄣ', 'ㄉㄚˋ', 'ㄕㄨˋ', 'ㄧ', 'ㄧㄤˋ','ㄍㄠ' ])
     end
   end
 
